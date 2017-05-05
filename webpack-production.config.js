feb3298224303d42b863dfaf1d376b8674517877
Kjartan Bjørset <kjartanbjorset@gmail.com>
2017-05-06 01:54:07 +0200

    build

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -5,42 +5,39 @@ const nodeModulesPath = path.resolve(__dirname, 'node_modules');
 const TransferWebpackPlugin = require('transfer-webpack-plugin');
 
 const config = {
-  entry: [path.join(__dirname, '/src/app/app.js')],
-  // Render source-map file for final build
+
+  entry: [
+    path.join(__dirname, '/src/app/app.js')
+  ],
+
   devtool: 'source-map',
-  // output config
+
   output: {
     path: buildPath, // Path of output file
     filename: 'app.js', // Name of output file
   },
+
   plugins: [
-    // Define production build to allow React to strip out unnecessary checks
     new webpack.DefinePlugin({
       'process.env':{
         'NODE_ENV': JSON.stringify('production')
       }
     }),
-    // Minify the bundle
     new webpack.optimize.UglifyJsPlugin({
       compress: {
-        // suppresses warnings, usually from module minification
-        warnings: false,
+        warnings: true,
       },
-    }),
-    // Allows error warnings but does not stop compiling.
+    }),    
     new webpack.NoErrorsPlugin(),
-    // Transfer Files
     new TransferWebpackPlugin([
       {from: 'www'},
     ], path.resolve(__dirname, 'src')),
   ],
   module: {
     loaders: [
-
       {
         test: /\.(jpg|png|svg)$/,
-        loader: 'file',
-        include: './img'
+        loader: 'file'
       },
       {
         test: /\.(js|jsx)$/,
